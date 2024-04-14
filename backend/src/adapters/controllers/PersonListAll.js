5d882ab1735ec66d47214c9e5fce050b49d39296
Lucas Moraes <lucas.moraes88@yahoo.com>
2024-04-14 14:18:36 -0300

    backend: list-all image to base64

--- backend/src/adapters/controllers/PersonListAll.js
+++ backend/src/adapters/controllers/PersonListAll.js
@@ -3,7 +3,14 @@ const {PersonRepository} = require("../repositories/PersonRepository.js");
 class PersonListAll {
   static async execute(req, res, next) {
     try {
-      const response = await PersonRepository.listAll();
+      let response = await PersonRepository.listAll();
+      response =  response.map((person) => {
+        return {
+          name: person.name,
+          image: person.image.toString('base64'),
+          wanted: person.wanted,
+        };
+      })
       return res.status(200).send(response);
     } catch (error) {
       next(error);
